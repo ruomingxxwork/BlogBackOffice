3bec325c08cf087294cb13cd18db9ce843ae8d98
MOzil272 <mozil272@gmail.com>
2019-03-19 21:52:19 -0400

    Add album and image model

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Blog, Category, Tag, Level
+from .models import Blog, Category, Tag, Level,Album,Image
 
 admin.site.register(Blog)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Level)
+admin.site.register(Album)
+admin.site.register(Image)
